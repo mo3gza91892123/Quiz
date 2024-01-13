d53396366c3679a937c91eff581bae40ee9b17e5
mo3gza91892123 <145809199+mo3gza91892123@users.noreply.github.com>
2024-01-13 20:11:56 +0200

    Update List Two.js

--- JS/List Two.js
+++ JS/List Two.js
@@ -1,13 +1,15 @@
 let conter =0;
 let search = document.getElementById("search");
+let searchclick = document.getElementById("searchclick");
 async function Api(){
     const datares = await fetch("./JS/json/D.json");
     const quation = await datares.json();
     return quation;
 }
+const array = Api();
 function getall() {
     document.getElementById("quizAns").innerHTML="";
-    Api().then(quation => quation.data.forEach(element => {
+    array.then(quation => quation.data.forEach(element => {
         conter +=1;
         document.getElementById("quizAns").innerHTML+=`
         <ul class="list-group" id='id-${conter}'onclick="this.style.opacity != '0.5' ? this.style.opacity = '0.5': this.style.opacity = '1';localStorage.setItem('clicker','${conter}')">
@@ -22,32 +24,54 @@ function getall() {
     `;
     }))
 }
-search.addEventListener("keyup",function(){
+searchclick.addEventListener("click",function(){
+    console.log(array)
     if(search.value.length == 0){
         document.querySelector(".logo").innerHTML="الاسالة";
         getall();
     }
-    else if(search.value.length > 1){
+    else if(search.value.length > 0){
         document.getElementById("quizAns").innerHTML="";
         let c = 0;
-        Api().then(quation => quation.data.forEach(element => {
-            if (element.q.search(search.value) >= 0|| element.o[element.a].search(search.value) >= 0) {   
-                c ++;
-                document.getElementById("quizAns").innerHTML+=`
+        // array.data.forEach(element => {
+        //     if (element.o.length > 1) {
+        //         data.push(element)
+        //     }
+        // });
+        array.then(quation => quation.data.forEach(element => {
+            if (element.o.length > 1) {
+                if (element.q.search(search.value) >= 0 || element.o[element.a].search(search.value) >= 0) {   
+                    c ++;
+                    document.getElementById("quizAns").innerHTML+=`
                     <ul class="list-group" onclick="this.style.opacity != '0.5' ? this.style.opacity = '0.5' : this.style.opacity = '1'">
-                    <li class="list-group-item active">السؤال : ${element.q}</li>
-                    <div class="row">
-                        <div class="col-md">
-                    <li class="list-group-item"> ${element.o.length > 2 ? element.o[0] + " - " + element.o[1] +" - "+ element.o[2] +" - "+ element.o[3] : element.o === 1 ? element.o[0]: element.o[0] + " - " + element.o[1]} </li>
+                        <li class="list-group-item active">السؤال : ${element.q}</li>
+                        <div class="row">
+                            <div class="col-md">
+                                <li class="list-group-item"> ${element.o.length > 2 ? element.o[0] + " - " + element.o[1] +" - "+ element.o[2] +" - "+ element.o[3] : element.o.length === 1 ? '': element.o[0] + " - " + element.o[1]} </li>
+                            </div>
                         </div>
-                    </div>
-                    <li class="list-group-item active">الاجابة : (${element.o[element.a] !=undefined ?element.o[element.a]: element.a})</li>
-                </ul>
+                        <li class="list-group-item active">الاجابة : (${element.o[element.a] !=undefined ?element.o[element.a]: element.a})</li>
+                    </ul>
                 `;
+                }
+            }else{
+                if (element.q.search(search.value) >= 0 || element.a.search(search.value) >= 0) {   
+                    c ++;
+                    document.getElementById("quizAns").innerHTML+=`
+                    <ul class="list-group" onclick="this.style.opacity != '0.5' ? this.style.opacity = '0.5' : this.style.opacity = '1'">
+                        <li class="list-group-item active text-success">السؤال : ${element.q}</li>
+                        <div class="row">
+                            <div class="col-md">
+                                <li class="list-group-item"> ${element.o.length > 2 ? element.o[0] + " - " + element.o[1] +" - "+ element.o[2] +" - "+ element.o[3] : element.o.length === 1 ? '': element.o[0] + " - " + element.o[1]} </li>
+                            </div>
+                        </div>
+                        <li class="list-group-item active text-success">الاجابة : (${element.o[element.a] !=undefined ?element.o[element.a]: element.a})</li>
+                    </ul>
+                `;
+                }
             }
             document.querySelector(".logo").innerHTML="يوجد " + c + " سؤال";
             }));
     }
 })
-// document.getElementById("q-1").onclick = document.getElementById("q-1").style.display = "none";
 getall();
