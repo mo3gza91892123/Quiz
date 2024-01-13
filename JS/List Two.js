2b82facaf525bd72918ddccdad937b1f7182169b
mo3gza91892123 <145809199+mo3gza91892123@users.noreply.github.com>
2024-01-13 20:41:17 +0200

    Update List Two.js

--- JS/List Two.js
+++ JS/List Two.js
@@ -1,3 +1,7 @@
+const Trues = document.getElementById("Trues");
+const Falses = document.getElementById("Falses");
+const Choos = document.getElementById("Choos");
+
 let conter =0;
 let search = document.getElementById("search");
 let searchclick = document.getElementById("searchclick");
@@ -24,8 +28,77 @@ function getall() {
     `;
     }))
 }
+Choos.addEventListener("click",function(){
+    document.getElementById("quizAns").innerHTML="";
+        let c = 0;
+        array.then(quation => quation.data.forEach(element => {
+            if (element.o.length > 1) {
+                if (element.o.length == 4) {   
+                    c ++;
+                    document.getElementById("quizAns").innerHTML+=`
+                    <ul class="list-group" onclick="this.style.opacity != '0.5' ? this.style.opacity = '0.5' : this.style.opacity = '1'">
+                        <li class="list-group-item active">السؤال : ${element.q}</li>
+                        <div class="row">
+                            <div class="col-md">
+                                <li class="list-group-item"> ${element.o.length > 2 ? element.o[0] + " - " + element.o[1] +" - "+ element.o[2] +" - "+ element.o[3] : element.o.length === 1 ? '': element.o[0] + " - " + element.o[1]} </li>
+                            </div>
+                        </div>
+                        <li class="list-group-item active">الاجابة : (${element.o[element.a] !=undefined ?element.o[element.a]: element.a})</li>
+                    </ul>
+                `;
+                }
+            }
+            document.querySelector(".logo").innerHTML="يوجد " + c + " سؤال";
+            }));
+});
+Trues.addEventListener("click",function(){
+    document.getElementById("quizAns").innerHTML="";
+        let c = 0;
+        array.then(quation => quation.data.forEach(element => {
+            if (element.o.length > 1) {
+                if (element.o.length == 2 && element.a == 1) {   
+                    c ++;
+                    document.getElementById("quizAns").innerHTML+=`
+                    <ul class="list-group" onclick="this.style.opacity != '0.5' ? this.style.opacity = '0.5' : this.style.opacity = '1'">
+                        <li class="list-group-item active">السؤال : ${element.q}</li>
+                        <div class="row">
+                            <div class="col-md">
+                                <li class="list-group-item"> ${element.o.length > 2 ? element.o[0] + " - " + element.o[1] +" - "+ element.o[2] +" - "+ element.o[3] : element.o.length === 1 ? '': element.o[0] + " - " + element.o[1]} </li>
+                            </div>
+                        </div>
+                        <li class="list-group-item active">الاجابة : (${element.o[element.a] !=undefined ?element.o[element.a]: element.a})</li>
+                    </ul>
+                `;
+                }
+            }
+            document.querySelector(".logo").innerHTML="يوجد " + c + " سؤال";
+            }));
+});
+
+Falses.addEventListener("click",function(){
+    document.getElementById("quizAns").innerHTML="";
+        let c = 0;
+        array.then(quation => quation.data.forEach(element => {
+            if (element.o.length > 1) {
+                if (element.o.length == 2 && element.a == 0) {
+                    c ++;
+                    document.getElementById("quizAns").innerHTML+=`
+                    <ul class="list-group" onclick="this.style.opacity != '0.5' ? this.style.opacity = '0.5' : this.style.opacity = '1'">
+                        <li class="list-group-item active">السؤال : ${element.q}</li>
+                        <div class="row">
+                            <div class="col-md">
+                                <li class="list-group-item"> ${element.o.length > 2 ? element.o[0] + " - " + element.o[1] +" - "+ element.o[2] +" - "+ element.o[3] : element.o.length === 1 ? '': element.o[0] + " - " + element.o[1]} </li>
+                            </div>
+                        </div>
+                        <li class="list-group-item active">الاجابة : (${element.o[element.a] !=undefined ?element.o[element.a]: element.a})</li>
+                    </ul>
+                `;
+                }
+            }
+            document.querySelector(".logo").innerHTML="يوجد " + c + " سؤال";
+            }));
+});
 searchclick.addEventListener("click",function(){
-    console.log(array)
     if(search.value.length == 0){
         document.querySelector(".logo").innerHTML="الاسالة";
         getall();
@@ -33,11 +106,6 @@ searchclick.addEventListener("click",function(){
     else if(search.value.length > 0){
         document.getElementById("quizAns").innerHTML="";
         let c = 0;
-        // array.data.forEach(element => {
-        //     if (element.o.length > 1) {
-        //         data.push(element)
-        //     }
-        // });
         array.then(quation => quation.data.forEach(element => {
             if (element.o.length > 1) {
                 if (element.q.search(search.value) >= 0 || element.o[element.a].search(search.value) >= 0) {   
@@ -55,5 +123,9 @@ searchclick.addEventListener("click",function(){
                 `;
                 }
             }
+            document.querySelector(".logo").innerHTML="يوجد " + c + " سؤال";
+            }));
+    }
 })
+// document.getElementById("q-1").onclick = document.getElementById("q-1").style.display = "none";
 getall();
